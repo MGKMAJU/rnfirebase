26b19485fd0fdff5d100686bb4fde68f043873a7
MGKMAJU <majustudent@jinnah.edu>
2019-04-21 19:54:44 +0500

    all done

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
- 
-
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
+import { View } from "react-native";
+// import the firebase third party lib
 import firebase from "firebase";
+// Custom Components to be used in the app
+import { Header } from "./components/common";
+// Import our LoginForm component to be displayed on the screen
+import LoginForm from "./components/LoginForm";
 
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
- 
-export default class App extends Component {
-    // Life cycle method to init the firebase
-    componentWillMount() {
-        firebase.initializeApp({
-            apiKey: "AIzaSyB_igHgA4NAOycx1Rk7TQM3yJebYKkTssc",
-            authDomain: "rnappauthclass.firebaseapp.com",
-            databaseURL: "https://rnappauthclass.firebaseio.com",
-            projectId: "rnappauthclass",
-            storageBucket: "rnappauthclass.appspot.com",
-            messagingSenderId: "1087736175930"
-        });
-    }
-  render() {
-    return (
-      <View  >
-        <Text>A Firebase Integration Test Application</Text>
-      </View>
-    );
-  }
+class App extends Component {
+// Life cycle method to init the firebase
+componentWillMount() {
+firebase.initializeApp({
+apiKey: "AIzaSyBsaXqqEioMPhYrSdSrSd3mCwK0sUHUeAc",
+authDomain: "auth-e7ac5.firebaseapp.com",
+databaseURL: "https://auth-e7ac5.firebaseio.com",
+projectId: "auth-e7ac5",
+storageBucket: "auth-e7ac5.appspot.com",
+messagingSenderId: "846221714758"
+});
+}
+render() {
+return (
+<View>
+<Header headerText="Authentication" />
+<LoginForm />
+</View>
+);
+}
 }
 
- 
+export default App;
